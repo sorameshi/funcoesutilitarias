ba3e242a1f3543d08d7d1fbe219990fd33b6e311
sulisuprevida <sulivan.leite@suprevida.com.br>
2021-08-18 11:24:40 -0300

    listando urls do banco e consultas diversas

--- comunicabd.py
+++ comunicabd.py
@@ -1,25 +1,23 @@
 import psycopg2
 import datetime
-from datetime import date
+import urllib.parse
+
 
 import json, os
 
 import logging
-
-def change_permissions_recursive(path, mode):
-    for root, dirs, files in os.walk(path, topdown=False):
-        for dir in [os.path.join(root,d) for d in dirs]:
-            os.chmod(dir, mode)
-    for file in [os.path.join(root, f) for f in files]:
-            os.chmod(file, mode)
+import azure.functions as func
 
 def init():
     logging.basicConfig(level=logging.DEBUG)
     # Update connection string information
-    host = os.environ['JDBC_HOST']
+    host = os.environ["JDBC_HOST"]
     dbname = os.environ["JDBC_NAME"]
     user = os.environ["JDBC_USER"]
     password = os.environ["JDBC_PASS"]
+
+    
+    # DOMAIN = os.environ["DOMAIN"]
 
     sslmode = "require"
 
@@ -42,48 +40,40 @@ def init():
         if isinstance(o, datetime.datetime):
             return o.__str__()
 
+    print("Carregando produtos...")
+    query_produtos_ativos = query_db("SELECT p.id, p.nome, Count(p.*) as quantidade, p.codigo as sku, m.nome as marca from produto_valor pv left join produto p on (p.id = pv.idproduto) left join lojista l on (pv.idlojista = l.id) left join marca m on (p.idmarca = m.id) where p.removido = false and pv.removido = false and pv.idproduto is not null and p.ativo = true and l.removido = false and p.nomeurl is not null GROUP BY p.id, p.nome, p.codigo, m.nome HAVING Count(*) > 1 order by p.nome asc")    
+    ids_produtos=[]
+    print('Populando a lista de ids das categorias principais!')
+    for item in query_produtos_ativos:
+        ids_produtos.append(item["id"])
     
-    #query = query_db("select alias from noticia where removido = false order by id")
-    query = query_db("select p.nomeurl as url from produto_valor pv left join produto p on (p.id = pv.idproduto) left join lojista l on (pv.idlojista = l.id) where pv.removido = false and pv.idproduto is not null and p.ativo = true and l.removido = false order by p.nomeurl asc")
-
-    json_union = {}
-    data_atual = date.today()
-    hoje = f'{data_atual.day}/{data_atual.month}/{data_atual.year}'
-
-    json_union['data_criacao'] = hoje
-    json_union['artigos']= query
     
-    links = []
-    filtro = 'produto'
+    print('Concluindo sitemap.txt')
+    urls = query_produtos_ativos
+    print(f'listagem concluida, resultado em formato lista de strings separado por virgula: \n{urls}')
 
-    #Concatenando URL
-    print('lista links!')
-    for item in query:
-        # if item["url"][0] != "'":
-        links.append(f'https://staging.suprevida.com.br/{filtro}/{item["url"]}')
-
-    links.sort()
-
-    links_ordenados = ','.join(str(e) for e in links)
+    f = open("duplicacoes.txt", "a")
+    f.write(str(urls))
+    f.close()
     
-    print('Convertendo para json e formatando!')
-    result = json.dumps(links_ordenados, default = date_converter, indent=4, ensure_ascii=False)
-
+    
     # Clean up
     cursor.close()
     conn.close()
 
-    # Create a file in the local data directory to upload and download
-    local_file_name = "result.json"
+    return urls
 
-    try:
-        with open(local_file_name, "w", encoding="utf-8") as myfile:
-            myfile.write(result)
-    except Exception as e:
-        print(e)
-        print(f"falha ao escrever no arquivo:\n\t{local_file_name}!")
-        
-    return result
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    if init():
+        return func.HttpResponse(json.dumps({'message': init()}), status_code=201, mimetype="application/json")
+    else:
+        return func.HttpResponse(
+            "Algo deu errado!",
+            status_code=400, 
+            mimetype="application/json"
+        )
 
 if __name__ == "__main__":
     init()
---
+++ utf8normalize.py
@@ -0,0 +1,6 @@
+title = 'á'
+
+# change encoding to utf-8
+print(title.encode())
+
+# Output: b'Python Programming'
